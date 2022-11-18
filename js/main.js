8b0144ab1eab1a4de7b62a8b7c66159bb729f4b4
lufranski <lufranski@gmail.com>
2022-11-18 17:02:37 +0100

    Reset

--- js/main.js
+++ js/main.js
@@ -1,24 +1,13 @@
 // Simon Says
 
 // Il computer genera 5 numeri casuali
-let simonNumbers = arrayGenerator(5);
-
-console.log(simonNumbers);
 
 // I numeri vengono stampati sulla pagina
-const wrapper = document.getElementById('simon-numbers');
 
-console.log(wrapper);
 
-// Ciclo per stampare i numeri in pagina tramite innerHtml
-for (let i = 0; i < simonNumbers.length; i++){
-    
-    wrapper.innerHTML += `<span class="number">${simonNumbers[i]}</span> `;
-
-}
 // Parte un timer di 30 secondi
+
 // A questo punto i 5 numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto
-const clock = setTimeout(() => wrapper.classList.add('invisible'), 3000);
 
 // Infine il computer comunica quanti e quali numeri sono stati indovinati
 
