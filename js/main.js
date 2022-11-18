397065da30f1eb686f8b06a62d0d9655e9b24d2e
lufranski <lufranski@gmail.com>
2022-11-18 18:47:54 +0100

    prompt & console.log

--- js/main.js
+++ js/main.js
@@ -1,15 +1,56 @@
+
+
+
 // Simon Says
 
 // Il computer genera 5 numeri casuali
+let simonNumbers = arrayGenerator(5);
 
-// I numeri vengono stampati sulla pagina
+console.log(simonNumbers);
+
+const simonWrapper = document.getElementById('simon-numbers');
+
+console.log(simonWrapper);
+
+simonWrapper.innerHTML = simonNumbers;
+// Parte un timer di 30 secondi 
+const timer = setTimeout(
+    function(){
+        for(let i = 0; i < 5; i++){
+            
+            let userNumber = parseInt(prompt('Inserisci uno dei numeri che hai visto ... '));
+
+            console.log(userNumber);
+            
+            let userArray = [];
+            
+
+            if (userNumber == simonNumbers[i]){
+                    
+                userArray.push(userNumber);
+                
+                let counter = userArray.length;
+                console.log(counter);
+    
+                console.log(userArray);
+    
+                console.log('Hai indovinato ' + counter + 'numeri');
+            }
 
 
-// Parte un timer di 30 secondi
+
+        }
+    }
+, 3000);
 
 // A questo punto i 5 numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto
 
-// Infine il computer comunica quanti e quali numeri sono stati indovinati
+// const clock = setTimeout(() => wrapper.classList.add('invisible'), 30000);
+
+let userArray = [];
+
+
+    
 
 
 
@@ -21,7 +62,7 @@
 function numberGenerator(){
     // Genera un numero random da 1 a 100
     return Math.floor((Math.random() * 100) + 1);
-
+    
 }
 
 function arrayGenerator(elementsNumber){
@@ -34,13 +75,16 @@ function arrayGenerator(elementsNumber){
         
         // Genera un numero random 
         let newNumber = numberGenerator();
-
+        
         // Lo pusha nell'array
         newArray.push(newNumber);
-
+        
     }
-
+    
     // Ritorna l'array
     return newArray;
 }
+
+
+
 
